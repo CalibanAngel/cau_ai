991ecd46dcd8739ba1291fa3bc6b3ded79cd1361
quiefh <lordcaliban@gmail.com>
2017-11-16 19:12:47 +0900

    first genetic algorithm

---
+++ genetic.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python3
+
+import random, sys, time
+
+
+class City:
+    name = None
+    distances = []
+
+    def __init__(self, name, distances):
+        self.name = name
+        self.distances = distances
+
+    def getName(self):
+        return self.name
+
+    def getDistance(self, name):
+        return self.distances[name]
+
+    def getDistances(self):
+        return self.distances
+
+
+class TourManager:
+    destinationCities = []
+
+    def addCity(self, city):
+        self.destinationCities.append(city)
+
+    def getCity(self, index):
+        return self.destinationCities[index]
+
+    def numberOfCities(self):
+        return len(self.destinationCities)
+
+
+class Tour:
+    def __init__(self, tourmanager, tour=None):
+        self.tourmanager = tourmanager
+        self.tour = []
+        self.fitness = 0.0
+        self.distance = 0
+        if tour is not None:
+            self.tour = tour
+        else:
+            for i in range(0, self.tourmanager.numberOfCities()):
+                self.tour.append(None)
+
+    def __len__(self):
+        return len(self.tour)
+
+    def __getitem__(self, index):
+        return self.tour[index]
+
+    def __setitem__(self, key, value):
+        self.tour[key] = value
+
+    def __repr__(self):
+        geneString = "|"
+        for i in range(0, self.tourSize()):
+            geneString += str(self.getCity(i)) + "|"
+        return geneString
+
+    def generateIndividual(self):
+        for cityIndex in range(0, self.tourmanager.numberOfCities()):
+            self.setCity(cityIndex, self.tourmanager.getCity(cityIndex))
+        random.shuffle(self.tour)
+
+    def getCity(self, tourPosition):
+        return self.tour[tourPosition]
+
+    def setCity(self, tourPosition, city):
+        self.tour[tourPosition] = city
+        self.fitness = 0.0
+        self.distance = 0
+
+    def getFitness(self):
+        if self.fitness == 0:
+            self.fitness = 1 / float(self.getDistance())
+        return self.fitness
+
+    def getDistance(self):
+        if self.distance == 0:
+            tourDistance = 0
+            for cityIndex in range(0, self.tourSize()):
+                fromCity = self.getCity(cityIndex)
+                destinationCity = None
+                if cityIndex + 1 < self.tourSize():
+                    destinationCity = self.getCity(cityIndex + 1)
+                else:
+                    destinationCity = self.getCity(0)
+                tourDistance += fromCity.getDistance(destinationCity.getName())
+            self.distance = tourDistance
+        return self.distance
+
+    def tourSize(self):
+        return len(self.tour)
+
+    def containsCity(self, city):
+        return city in self.tour
+
+
+class Population:
+    def __init__(self, tourmanager, populationSize, initialise):
+        self.tours = []
+        for i in range(0, populationSize):
+            self.tours.append(None)
+
+        if initialise:
+            for i in range(0, populationSize):
+                newTour = Tour(tourmanager)
+                newTour.generateIndividual()
+                self.saveTour(i, newTour)
+
+    def __setitem__(self, key, value):
+        self.tours[key] = value
+
+    def __getitem__(self, index):
+        return self.tours[index]
+
+    def saveTour(self, index, tour):
+        self.tours[index] = tour
+
+    def getTour(self, index):
+        return self.tours[index]
+
+    def getFittest(self):
+        fittest = self.tours[0]
+        res = ""
+        for i in range(0, self.populationSize()):
+            if fittest.getFitness() <= self.getTour(i).getFitness():
+                fittest = self.getTour(i)
+            res += (str(self.getTour(i).getDistance()) + " | ")
+        return fittest
+
+    def populationSize(self):
+        return len(self.tours)
+
+
+class GA:
+    def __init__(self, tourmanager):
+        self.tourmanager = tourmanager
+        self.mutationRate = 0.015
+        self.tournamentSize = 5
+        self.elitism = True
+
+    def evolvePopulation(self, pop):
+        newPopulation = Population(self.tourmanager, pop.populationSize(), False)
+        elitismOffset = 0
+        if self.elitism:
+            newPopulation.saveTour(0, pop.getFittest())
+            elitismOffset = 1
+
+        for i in range(elitismOffset, newPopulation.populationSize()):
+            parent1 = self.tournamentSelection(pop)
+            parent2 = self.tournamentSelection(pop)
+            child = self.crossover(parent1, parent2)
+            newPopulation.saveTour(i, child)
+
+        for i in range(elitismOffset, newPopulation.populationSize()):
+            self.mutate(newPopulation.getTour(i))
+
+        return newPopulation
+
+    def crossover(self, parent1, parent2):
+        child = Tour(self.tourmanager)
+
+        startPos = int(random.random() * parent1.tourSize())
+        endPos = int(random.random() * parent1.tourSize())
+
+        for i in range(0, child.tourSize()):
+            if startPos < endPos and i > startPos and i < endPos:
+                child.setCity(i, parent1.getCity(i))
+            elif startPos > endPos:
+                if not (i < startPos and i > endPos):
+                    child.setCity(i, parent1.getCity(i))
+
+        for i in range(0, parent2.tourSize()):
+            if not child.containsCity(parent2.getCity(i)):
+                for ii in range(0, child.tourSize()):
+                    if child.getCity(ii) == None:
+                        child.setCity(ii, parent2.getCity(i))
+                        break
+
+        return child
+
+    def mutate(self, tour):
+        for tourPos1 in range(0, tour.tourSize()):
+            if random.random() < self.mutationRate:
+                tourPos2 = int(tour.tourSize() * random.random())
+
+                city1 = tour.getCity(tourPos1)
+                city2 = tour.getCity(tourPos2)
+
+                tour.setCity(tourPos2, city1)
+                tour.setCity(tourPos1, city2)
+
+    def tournamentSelection(self, pop):
+        tournament = Population(self.tourmanager, self.tournamentSize, False)
+        for i in range(0, self.tournamentSize):
+            randomId = int(random.random() * pop.populationSize())
+            tournament.saveTour(i, pop.getTour(randomId))
+        fittest = tournament.getFittest()
+        return fittest
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.exit()
+    inputFileName = sys.argv[1]
+    outputFileName = sys.argv[2]
+
+    tourmanager = TourManager()
+
+    # Create and add our cities
+    input_file = open(inputFileName, 'r')
+    index = 0
+    for line in input_file:
+        tmp = City(index, list(map(int, line.split(","))))
+        tourmanager.addCity(tmp)
+        index += 1
+    input_file.close()
+
+    startTime = time.time()
+
+    # Initialize population
+    pop = Population(tourmanager, 50, True)
+    print("Initial distance: " + str(pop.getFittest().getDistance()))
+
+    # Evolve population for 50 generations
+    ga = GA(tourmanager)
+    pop = ga.evolvePopulation(pop)
+    for i in range(0, 10):
+        pop = ga.evolvePopulation(pop)
+
+    # Print final results
+    print("Finished")
+    print("Final distance: " + str(pop.getFittest().getDistance()))
+    print("Total time: " + str(time.time() - startTime))
+
+    res = pop.getFittest()
+    outputFile = open(outputFileName, 'w')
+    for i in range(len(res)):
+        elem = res[i].getName()
+        outputFile.write(str(elem + 1) + '\n')
+    outputFile.close()
+
